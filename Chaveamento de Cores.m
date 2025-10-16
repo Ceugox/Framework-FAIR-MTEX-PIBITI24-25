8c199bcbc6daa4ad3d5832996edc7bdbdba47774
Marcell Parra <143279940+Ceugox@users.noreply.github.com>
2025-10-16 09:18:01 -0300

    Update and rename Chaveamento de Cores to Chaveamento de Cores.m

--- Chaveamento de Cores.m
+++ Chaveamento de Cores.m
@@ -1,5 +1,5 @@
 % Define a chave de cores IPF para a fase Cu eletrolitico
-oM = ipfColorKey ( ebsd ( 'Cu eletrolitico ')) ;
+oM = ipfColorKey ( ebsd ( 'Cu eletrolitico')) ;
 % Plota a chave de cores IPF
 figure;
 plot ( oM );
