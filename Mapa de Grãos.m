b4a9261608635b280ac026f01072e1b71f105a8b
Marcell Parra <143279940+Ceugox@users.noreply.github.com>
2025-10-16 09:20:26 -0300

    Fix comments and variable access in Mapa de Grãos.m

--- Mapa de Grãos.m
+++ Mapa de Grãos.m
@@ -1,13 +1,13 @@
-% Calcula os grãos com um ngulo de desorienta ção de 15 graus ( ajustável )
-[ grains , ebsd . grainId ] = calcGrains ( ebsd ( 'indexed') , 'angle ', 15*degree ) ;
+% Calcula os grãos com um ngulo de desorientação de 15 graus ( ajustável )
+[ grains , ebsd.grainId ] = calcGrains ( ebsd ( 'indexed') , 'angle', 15*degree ) ;
 % Plota os grãos calculados
 figure ;
 plot ( grains , 'linewidth', 0.5) ;
 title ('Grãos Calculados - ângulo de 15 Graus ');
 % Remove grãos menores que 3 pixels ( ajustável )
 ebsd ( grains ( grains . grainSize < 3) ) = [];
-% Recalcula os grãos com um ngulo de desorienta ção de 10 graus (ajustável )
-[ grains , ebsd . grainId ] = calcGrains ( ebsd ( 'indexed ') , 'angle ', 10*degree ) ;
+% Recalcula os grãos com um ngulo de desorientação de 10 graus (ajustável )
+[ grains , ebsd.grainId ] = calcGrains ( ebsd ( 'indexed') , 'angle', 10*degree ) ;
 % Suaviza as bordas dos grãos com raio 2 ( ajustável )
 grains = smooth ( grains , 2) ;
 % Plota os grãos suavizados
