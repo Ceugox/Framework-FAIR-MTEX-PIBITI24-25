469a3e8b110cce482d477640de5bce045df8a586
Marcell Parra <143279940+Ceugox@users.noreply.github.com>
2025-11-28 16:31:58 -0300

    Change grain calculation angle to 15 degrees
    Updated the angle for grain calculation and adjusted plot titles accordingly.

--- Mapa de Grãos.m
+++ Mapa de Grãos.m
@@ -7,10 +7,16 @@ title ('Grãos Calculados - ângulo de 15 Graus ');
 % Remove grãos menores que 3 pixels ( ajustável )
 ebsd ( grains ( grains . grainSize < 3) ) = [];
 % Recalcula os grãos com um ngulo de desorientação de 10 graus (ajustável )
-[ grains , ebsd.grainId ] = calcGrains ( ebsd ( 'indexed') , 'angle', 10*degree ) ;
+[ grains , ebsd.grainId ] = calcGrains ( ebsd ( 'indexed') , 'angle', 15*degree ) ;
 % Suaviza as bordas dos grãos com raio 2 ( ajustável )
 grains = smooth ( grains , 2) ;
 % Plota os grãos suavizados
 figure ;
 plot ( grains , 'linewidth', 0.5) ;
 title ('Grãos Suavizados - ângulo de 10 Graus ');
+ipfKey = ipfColorKey(ebsd('Copper'));
+ipfKey.inversePoleFigureDirection = vector3d.Z;
+
+% this is the colored fundamental sector
+plot(ipfKey)
+
